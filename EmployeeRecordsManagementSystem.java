0958157b26fd9d41f48fa6520261d6bc690dc15e
AbhayGodara <AbhayGodara@RIA-IN-557>
2023-03-09 16:30:40 +0530

    Made changes to employee class

--- EmployeeRecordsManagementSystem.java
+++ EmployeeRecordsManagementSystem.java
@@ -2,6 +2,18 @@ package employeeManagement;
 
 
 public class EmployeeRecordsManagementSystem {
+	private static Connection conn;
+    
+    public EmployeeRecordsManagementSystem() throws SQLException {
+        // Initialize the database connection
+    	private static String DB_URL = "jdbc:oracle:thin:@//localhost:1521/orcl";
+    	private static String USER = "CISADM";
+    	private static String PASS = "CISADM";
+        conn = DriverManager.getConnection(DB_URL, USER, PASS);
+    }
+    
+    
+	
 	public static void main(String args[]) {
 		
 	}
